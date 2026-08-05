177a761d6a478944f25a00925049a5ffa91d5eff
Hani-hue <isnahabibah9827@gmail.com>
2026-08-05 14:34:46 +0700

    feat: tambahkan definisi model Komik dengan atribut yang diperlukan

--- models/komik.js
+++ models/komik.js
@@ -1,1 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        sinopsis: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        penulis_Id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
